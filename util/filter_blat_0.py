7193bb509c079a746f567b1820b41e94e5fdbaf3
Tianyang Li <tmy1018@gmail.com>
2012-07-24 11:59:43 +0800

    jgoiwejrioij

--- util/filter_blat_0.py
+++ util/filter_blat_0.py
@@ -15,26 +15,4 @@
 #
 #  You should have received a copy of the GNU General Public License
 
-import random
 
-def main():
-    s_out = open('s.fasta', 'w')  # subject
-    q_out = open('q.fastq', 'w')  # query reads
-    
-    s1 = "".join(random.choice(["A", "C", "G", "T"]) for _ in xrange(200))
-    s2 = "".join(random.choice(["A", "C", "G", "T"]) for _ in xrange(200))
-    
-    s_out.write(">1\n%s\n" % s1)
-    s_out.write(">2\n%s\n" % s2)
-    
-    qual_str = "~" * 100
-    
-    for q_start in xrange(100):
-        q_seq = "%s%s" % (s1[q_start:100], s2[100:100 + q_start])
-        q_out.write("@q%d\n%s\n+\n%s\n" % (q_start, q_seq, qual_str))
-    
-    s_out.close()
-    q_out.close()
-
-if __name__ == '__main__':
-    main()
