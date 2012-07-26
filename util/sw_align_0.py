eb79f4f9d635c306d9154487596a585cfbca3710
Tianyang Li <tmy1018@gmail.com>
2012-07-26 22:58:38 +0800

    boitrgjotrigjp

--- util/sw_align_0.py
+++ util/sw_align_0.py
@@ -15,19 +15,4 @@
 #
 #  You should have received a copy of the GNU General Public License
 
-import sys
-import getopt
-
-from Bio import SeqIO
-
-
-
-def main():
-    contig_file = None
-    all_read_file = None
-    blat_reads = None  # reads that are fed to blat to align
-    ua_reads = None  # reads unaligned by bowtie and blat 
-
-if __name__ == '__main__':
-    main()
 
