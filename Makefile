1c2aaee186ba43714d294fa3b9fa15a9d76bf029
Tianyang Li <tmy1018@gmail.com>
2012-07-27 15:00:21 +0800

    gmewrgmopwgmporwboprembopremotpmowrbhowjor[p

---
+++ Makefile
@@ -0,0 +1,5 @@
+CC = g++
+CFLAGS = -W -Wall -Wno-long-long -pedantic -Wno-variadic-macros
+LIBS = -lrt
+
+all:
--- util/__init__.py
+++ util/__init__.py
@@ -13,6 +13,6 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
+#  You should have received a copy of the GNU General Public Licens
 
 
