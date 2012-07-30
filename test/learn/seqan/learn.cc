a04eae1ea9536ab7fa6b00ac733eeeb233af105f
Tianyang Li <tmy1018@gmail.com>
2012-07-30 16:31:12 +0800

    gnvewoirgnon

---
+++ test/learn/seqan/learn.cc
@@ -0,0 +1,12 @@
+// Tianyang Li 2012
+// tmy1018@gmail.com
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "learn.h"
+
+int main(int argc, char **argv) {
+	return 0;
+}
+
---
+++ test/learn/seqan/learn.h
@@ -0,0 +1,9 @@
+// Tianyang Li 2012
+// tmy1018@gmail.com
+
+#ifndef LEARN_H_
+#define LEARN_H_
+
+#include <seqan/align.h>
+
+#endif // LEARN_H_
